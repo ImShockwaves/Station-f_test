bf0ce88fc9afddcd0cbfc9a96556883c4706bc51
Camille Charrier <ccharrie@student.42.fr>
2019-03-25 01:58:45 +0100

    finished room design

--- public/Vuejs/index.js
+++ public/Vuejs/index.js
@@ -2,7 +2,35 @@ const logo = new Vue({
     el: '#app',
     data:
     {
-        active: false
+        rooms: [
+            {
+                name: 'roomA',
+                capacity: 10,
+                equipements : ['Tv', 'retro projecteur']
+            },
+            {
+                name: 'roomB',
+                capacity: 28,
+                equipements : ['Tv']
+            },
+            {
+                name: 'roomC',
+                capacity: 5,
+                equipements : null
+            }
+        ],
+        active: false,
+        dropdown: false,
+        inArray: function(needle, haystack) {
+            if (haystack)
+            {
+                var length = haystack.length;
+                for(var i = 0; i < length; i++)
+                    if(haystack[i] == needle)
+                        return true;
+            }
+            return false;
+        }
     },
 
     computed: 
@@ -13,6 +41,28 @@ const logo = new Vue({
                 return {'background-color': '#fc1cad'}
             else
                 return {'background-color': 'black'}
+        },
+        setHideOrReaveal: function ()
+        {
+            if (this.dropdown)
+            {
+                return {
+                    'clear': 'both',
+                    'display': 'block',
+                    'height': '175%',
+                    'width': '105%',
+                    'position': 'absolute',
+                    'border-style': 'solid',
+                    'border-color': '#c8ccd0',
+                    'border-radius': '5px',
+                    'border-width': '1px',
+                    'background-color': 'white',
+                    'padding-left': '3%',
+                    'padding-top': '2%',
+                    'margin-top': '2%'}
+            }
+             else
+                 return {'display': 'none'}
         }
     }
 });
