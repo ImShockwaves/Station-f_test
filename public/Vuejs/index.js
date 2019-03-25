656c448bfe94ac5462358b188c0007dab06d5338
Camille CHARRIER <ccharrie@e3r10p21.42.fr>
2019-03-25 17:35:24 +0100

    almost finished front

--- public/Vuejs/index.js
+++ public/Vuejs/index.js
@@ -25,12 +25,31 @@ const logo = new Vue({
             if (haystack)
             {
                 var length = haystack.length;
-                for(var i = 0; i < length; i++)
-                    if(haystack[i] == needle)
+                for (var i = 0; i < length; i++)
+                    if( haystack[i] == needle)
                         return true;
             }
             return false;
-        }
+        },
+        stuffRequire: [],
+        capacityRequire: 0,
+        checkRoom: function(stuff, capacity) {
+            var ret = true;
+            if (capacity < this.capacityRequire && this.capacityRequire != 0)
+                ret = false;
+            if (this.stuffRequire.length > 0)
+            {
+                if (stuff == null)
+                    return false;
+                var length = this.stuffRequire.length;
+                for (var i = 0; i < length; i++)
+                    if (!this.inArray(this.stuffRequire[i], stuff))
+                        return false;
+            }
+            return ret;
+        },
+        selectedRoom: null,
+        date: null,
     },
 
     computed: 
@@ -63,6 +82,27 @@ const logo = new Vue({
             }
              else
                  return {'display': 'none'}
+        },
+        formIsValid: function()
+        {
+            return this.selectedRoom && this.date;
+        },
+        submitButtonColor: function()
+        {
+            if (this.formIsValid)
+              return '#4c7ef3';
+            else
+              return 'gray';
+        }   
+    },
+
+    methods:
+    {
+        selected: function (roomName) {
+            if (roomName == this.selectedRoom)
+                return {
+                    'border-width': '4px',
+                    'border-color': '#4c7ef3'}
         }
     }
 });
