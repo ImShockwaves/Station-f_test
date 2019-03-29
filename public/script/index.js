275198bc2f20d29ac41ba2a1035e5cd262fc85b9
Camille CHARRIER <ccharrie@e2r9p14.42.fr>
2019-03-29 18:33:27 +0100

    styling confirmation done

--- public/script/index.js
+++ public/script/index.js
@@ -5,36 +5,12 @@ const logo = new Vue({
         rooms: [],
         active: false,
         dropdown: false,
-        inArray: function(needle, haystack) {
-            if (haystack)
-            {
-                var length = haystack.length;
-                for (var i = 0; i < length; i++)
-                    for (var name in haystack[i])
-                        if (haystack[i][name] === needle)
-                            return true;
-            }
-            return false;
-        },
         stuffRequire: [],
         capacityRequire: 0,
-        checkRoom: function(stuff, capacity) {
-            var ret = true;
-            if (capacity < this.capacityRequire && this.capacityRequire != 0)
-                ret = false;
-            if (this.stuffRequire.length > 0)
-            {
-                if (stuff == null)
-                    return false;
-                var length = this.stuffRequire.length;
-                for (var i = 0; i < length; i++)
-                    if (!this.inArray(this.stuffRequire[i], stuff))
-                        return false;
-            }
-            return ret;
-        },
+        confirmRoom: false,
         selectedRoom: null,
         date: null
+        
     },
 
     computed: 
@@ -52,10 +28,10 @@ const logo = new Vue({
             {
                 return {
                     'clear': 'both',
-                    'display': 'block',
                     'height': '175%',
                     'width': '105%',
                     'position': 'absolute',
+                    'display': 'inline-block',
                     'border-style': 'solid',
                     'border-color': '#c8ccd0',
                     'border-radius': '5px',
@@ -81,7 +57,31 @@ const logo = new Vue({
               return '#4c7ef3';
             else
               return 'gray';
-        }   
+        },   
+        submitted: function ()
+        {
+            if (this.confirmRoom)
+            {
+                return {
+                    'position': 'absolute',
+                    'display': 'flex',
+                    'flex-direction': 'column',
+                    'justify-content': 'space-between',
+                    'z-index': '1',
+                    'width': '50vw',
+                    'height': '50vw',
+                    'top': '25%',
+                    'right': '25%',
+                    'padding': '2vw',
+                    'background-color': 'white',
+                    'border-style': 'solid',
+                    'border-width': '4px',
+                    'border-color': 'rgba(76, 126, 243, .5)',
+                    'border-radius': '5px' }
+            }
+            else
+                return { 'display': 'none'}
+        }
     },
 
     methods:
@@ -92,6 +92,32 @@ const logo = new Vue({
                     'border-width': '4px',
                     'border-color': '#4c7ef3'}
         },
+        inArray: function(needle, haystack) {
+            if (haystack)
+            {
+                var length = haystack.length;
+                for (var i = 0; i < length; i++)
+                    for (var name in haystack[i])
+                        if (haystack[i][name] === needle)
+                            return true;
+            }
+            return false;
+        },
+        checkRoom: function(stuff, capacity) {
+            var ret = true;
+            if (capacity < this.capacityRequire && this.capacityRequire != 0)
+                ret = false;
+            if (this.stuffRequire.length > 0)
+            {
+                if (stuff == null)
+                    return false;
+                var length = this.stuffRequire.length;
+                for (var i = 0; i < length; i++)
+                    if (!this.inArray(this.stuffRequire[i], stuff))
+                        return false;
+            }
+            return ret;
+        },
         setSelect: function (room)
         {
             if (this.date)
@@ -101,13 +127,16 @@ const logo = new Vue({
             }
         }
     },
-    mounted: function () {
+    mounted: function ()
+    {
         var self = this;
         $.ajax({
             url: '../ressources/rooms.json',
             method: 'GET',
             success: function (data) {
                 self.rooms = data.rooms;
+                for (var id = 0; id < self.rooms.length; id++)
+                    self.rooms[id].id = id; 
             },
             error: function (error) {
                 console.log(error);
