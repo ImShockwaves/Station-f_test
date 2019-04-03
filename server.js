9c859e0019d8586bb2433b318b9b4f3a3e55b117
Camille CHARRIER <ccharrie@e2r4p18.42.fr>
2019-04-03 16:19:04 +0200

    creating adding request on post

--- server.js
+++ server.js
@@ -8,31 +8,40 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-MongoClient.connect("mongodb://localhost/Station_F", function(error, db) {
-    if (error) return funcCallback(error);
-
-    console.log("Connecté à la base de données 'tutoriel'");
-});
-
 app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res, next){
-  res.render('./public/index.html');
+	res.render('./public/index.html');
 });
 
 app.post('/endpoint', function(req, res){
-  fs.readFile('public/api/reservation.json', 'utf8', function readFileCallback(err, data)
-  {
-    if (err)
-        console.log(err);
-    else
-    {
-      var obj = JSON.parse(data);
-      obj.reservation.push({'name': req.body.name, 'date': req.body.date});
-      json = JSON.stringify(obj);
-      fs.writeFile('public/api/reservation.json', json, 'utf8', (err) => { console.log(err); });
-    }
-  });
-  res.send(req.body);
+	fs.readFile('public/api/reservation.json', 'utf8', function readFileCallback(err, data)
+	{
+		if (err)
+				console.log(err);
+		else
+		{
+			var obj = JSON.parse(data);
+			var newobj = {'name': req.body.name, 'date': req.body.date};
+			obj.reservation.push(newobj);
+			json = JSON.stringify(obj);
+			fs.writeFile('public/api/reservation.json', json, 'utf8', (err) => { console.log(err); });
+			MongoClient.connect("mongodb://localhost/Station_F", function(error, db) {
+				if (error)
+					return funcCallback(error);
+				if (!db.getCollectionNames().find("reservation"))
+					db.createCollection("reservation");
+				else
+				{
+					db.collection("reservation").insert(newobj, null, function (error, results) {
+						if (error)
+							throw error;
+						console.log("Le document a bien été inséré");    
+					});
+				}
+			});
+		}
+	});
+	res.send(req.body);
 });
 
 app.listen(8333);
