05ddb1ff7d5541f3511d23a2e7e50ab242b9fb36
Camille CHARRIER <ccharrie@e2r4p18.42.fr>
2019-04-03 15:32:35 +0200

    init db with mongo

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var MongoClient = require("mongodb").MongoClient;
 var fs = require('fs');
  
 var app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+MongoClient.connect("mongodb://localhost/Station_F", function(error, db) {
+    if (error) return funcCallback(error);
+
+    console.log("Connecté à la base de données 'tutoriel'");
+});
 
 app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res, next){
